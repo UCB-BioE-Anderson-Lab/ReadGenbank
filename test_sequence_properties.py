95508b55ac38bf41e310be6bdee3788aca80a0de
J. Christopher Anderson <jcanderson2167@gmail.com>
2023-12-22 13:52:53 -0800

    added benchling seq

--- test_sequence_properties.py
+++ test_sequence_properties.py
@@ -4,19 +4,23 @@ import pytest
 from ReadGenbank import ReadGenbank
 import json
 
-def test_circular_sequence_p20N31():
+def test_circular_sequence_from_ApE():
     reader = ReadGenbank()
     result_json = reader.run("p20N31.ape", [])
     result = json.loads(result_json)
     assert result["circular_linear"] == "circular"
     assert result["length"] == 2911
 
-def test_linear_sequence_SCU49845():
+def test_linear_sequence_from_NCBI():
     reader = ReadGenbank()
     result_json = reader.run("SCU49845.gb", [])
     result = json.loads(result_json)
     assert result["circular_linear"] == "linear"
     assert result["length"] == 5028
 
-# Additional tests can be added for other sequences and properties
-#  Add one from benchling
+def test_linear_sequence_from_benchling():
+    reader = ReadGenbank()
+    result_json = reader.run("pbr322_egfr.gb", [])
+    result = json.loads(result_json)
+    assert result["circular_linear"] == "circular"
+    assert result["length"] == 5968
