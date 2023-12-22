0f7b57edf5452b379a06ece80fbe681c71600cad
J. Christopher Anderson <jcanderson2167@gmail.com>
2023-12-22 14:05:03 -0800

    minor

--- ReadGenbank.py
+++ ReadGenbank.py
@@ -2,7 +2,32 @@ from Bio import SeqIO
 import json
 
 class ReadGenbank:
-    def run(self, file_path, data_types, sequence_range=None):
+    """
+    Parses a GenBank file and returns specified data as a JSON string.
+    """
+    def run(self, file_path, data_types=None, sequence_range=None):
+        """
+        Parses a GenBank file and returns specified data based on data types and sequence range.
+
+        Parameters:
+        file_path (str): Path to the GenBank file. Supported extensions: .str, .ape, .gb, .seq.
+        data_types (list of str): Data types to extract (e.g., 'full_sequence', 'features').
+        sequence_range (tuple of int, optional): Start and end positions of the sequence range.
+
+        Returns:
+        str: JSON string containing requested data.
+            - 'circular_linear': 'circular' or 'linear', indicating the topology of the sequence.
+            - 'length': Total length of the sequence.
+            - 'full_sequence': The specified range of the DNA sequence.
+            - 'features': List of features within the specified range, each as a dictionary.
+
+        Raises:
+        ValueError: If file extension is not supported or range exceeds sequence length.
+        """
+        if data_types is None:
+            data_types = []  # Set a default value if none provided
+
+
         # Validate file extension
         if not file_path.endswith(('.str', '.ape', '.gb', '.seq')):
             raise ValueError("Invalid file extension. Supported extensions are .str, .ape, .gb, and .seq.")
--- test_sequence_properties.py
+++ test_sequence_properties.py
@@ -4,13 +4,6 @@ import pytest
 from ReadGenbank import ReadGenbank
 import json
 
-def test_circular_sequence_from_ApE():
-    reader = ReadGenbank()
-    result_json = reader.run("p20N31.ape", [])
-    result = json.loads(result_json)
-    assert result["circular_linear"] == "circular"
-    assert result["length"] == 2911
-
 def test_linear_sequence_from_NCBI():
     reader = ReadGenbank()
     result_json = reader.run("SCU49845.gb", [])
@@ -18,9 +11,23 @@ def test_linear_sequence_from_NCBI():
     assert result["circular_linear"] == "linear"
     assert result["length"] == 5028
 
+def test_circular_sequence_from_ApE():
+    reader = ReadGenbank()
+    result_json = reader.run("p20N31.ape", [])
+    result = json.loads(result_json)
+    assert result["circular_linear"] == "circular"
+    assert result["length"] == 2911
+
 def test_linear_sequence_from_benchling():
     reader = ReadGenbank()
     result_json = reader.run("pbr322_egfr.gb", [])
     result = json.loads(result_json)
     assert result["circular_linear"] == "circular"
     assert result["length"] == 5968
+
+def test_linear_sequence_no_datatype():
+    reader = ReadGenbank()
+    result_json = reader.run("SCU49845.gb")
+    result = json.loads(result_json)
+    assert result["circular_linear"] == "linear"
+    assert result["length"] == 5028
